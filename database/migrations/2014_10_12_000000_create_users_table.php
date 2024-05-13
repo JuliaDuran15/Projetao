596215903f5498c84373b842d06323d89161fc81
Julia Duran <111252981+JuliaDuran15@users.noreply.github.com>
2024-05-13 12:43:32 -0300

    tenta

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('cliente'); // Define um valor padrão, por exemplo, 'cliente'
             $table->rememberToken();
             $table->timestamps();
         });
